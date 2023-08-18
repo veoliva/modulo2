75afb8307eda7079a22bd7b7bf72af8c84130d1b
veoliva <veolivatorre@gmail.com>
2023-08-17 23:10:28 -0300

    modif js

---
+++ Desktop/backup version/proyectoGrupal v12/grupoDmodulo2/ejercicios.js
@@ -0,0 +1,39 @@
+ //clase14 ejercicios
+ 
+ //ejercicio 1
+
+let nombre = "veronica";
+let apellido = "oliva";
+let edad = 36;
+let pasatiempos = ["pesca", "lettering", "diseño"];
+let pareja = true;
+let soltera = false;
+
+//ejercicio 2
+
+let primerNumero = 10;
+let segundoNumero = 20;
+let suma = primerNumero + segundoNumero;
+
+console.log(suma);
+
+
+//ejercicio 3
+
+let botellaReutilizable = {
+    marca: "Contigo",
+    modelo: "botella deportiva",
+    año: 2023,
+    precio: 17000,
+    oferta: true,
+    publicidad: function (){
+        console.log("la mejor botella reutilizable termica del mercado")
+
+    }
+}
+
+console.log (botellaReutilizable.marca)
+console.log (botellaReutilizable)
+
+
+
