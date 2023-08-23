221140a98d004e3dc3f79c10a0fe07ab316a2503
veoliva <veolivatorre@gmail.com>
2023-08-22 21:10:59 -0300

    agregamos ejercicios clase15

--- Desktop/backup version/proyectoGrupal v12/grupoDmodulo2/ejercicios.js
+++ Desktop/backup version/proyectoGrupal v12/grupoDmodulo2/ejercicios.js
@@ -2,7 +2,7 @@
  
  //ejercicio 1
 
-let nombre = "veronica";
+/*let nombre = "veronica";
 let apellido = "oliva";
 let edad = 36;
 let pasatiempos = ["pesca", "lettering", "diseño"];
@@ -33,16 +33,48 @@ let botellaReutilizable = {
 }
 
 console.log (botellaReutilizable.marca)
-console.log (botellaReutilizable)
+console.log (botellaReutilizable) */
 
 
 
 //clase 15
 
 //ejercicio 1
+/*let nombre = prompt ("ingrese su nombre ");
+let edad = prompt ("ingrese su edad ");
+
+alert ("su nombre es" + nombre + "y tiene" + edad + "años");
+console.log("ingrese su nombre y edad" + nombre + edad);
 
 //ejercicio 2
+let nombre1 = prompt ("ingrese su nombre")
+let colorPreferido = prompt ("ingrese su color favorito")
+let comidaFavorita = prompt ("ingrese su comida favorita")
+let mejorPelicula = prompt ("ingrese su pelicula favorita")
+let trabajoActual = prompt ("ingrese su trabajo actual")
+
+let presentacion = `Hola, mi nombre es ${nombre1}. Mi color preferido es ${colorPreferido},
+mi comida favorita es ${comidaFavorita}, y considero que la mejor película es ${mejorPelicula}.
+Actualmente, trabajo como ${trabajoActual}.`;
+
+console.log (presentacion)
 
 //ejercicio 3
 
+alert(`Bienvenido/a a la web, ${nombre1}`); 
+*/
+
 //ejercicio 4
+
+let numeroAleatorio = parseInt(prompt("Ingrese un número aleatorio:"));
+
+let esPar = numeroAleatorio % 2 === 0;
+
+if (esPar) {
+    console.log(`${numeroAleatorio} es un número par.`);
+
+  } else {
+    console.log(`${numeroAleatorio} es un número impar.`);
+  }
+
+ 
